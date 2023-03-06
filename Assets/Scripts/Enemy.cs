915034a053b5ba272529955f79ab44f4374172a3
danbqhe <dangbqhe153275@fpt.edu.vn>
2023-03-06 17:06:35 +0700

    enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public Rigidbody2D target;
+
+    bool isLive = true;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (!isLive)
+             return;
+            Vector2 dirVec = target.position - rigid.position;
+            Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+            rigid.MovePosition(rigid.position + nextVec);
+            rigid.velocity = Vector2.zero;
     }
+
+    void LateUpdate()
+    {
+        if (!isLive)
+        return;
+            spriter.flipX = target.position.x < rigid.position.x;
+                
+    }
+
 }
--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 public class Reposition : MonoBehaviour
 {
     Player player;
+    Collider2D coll;
+
+    private void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
     void OnTriggerExit2D(Collider2D collision)
     {
         if (!collision.CompareTag("Area"))
@@ -40,7 +46,10 @@ public class Reposition : MonoBehaviour
                 break;
 
             case "Enemy":
-
+            if(coll.enabled)
+                {
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f,3f), Random.Range(-3f, 3f),0f));
+                }
                 break;
         }
 
